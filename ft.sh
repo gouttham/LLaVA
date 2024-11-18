506d8a8ca07783e909024502f6794d6ffc72f38a
gna23 <gna23@sfu.ca>
2024-11-18 11:51:44 -0800

    init

--- ft.sh
+++ ft.sh
@@ -1,4 +1,4 @@
-deepspeed ./llava/train/train_mem.py \
+deepspeed /localscratch/gna23/LLaVA/llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --lora_enable True \
     --lora_r 128 \
@@ -7,9 +7,9 @@ deepspeed ./llava/train/train_mem.py \
     --bits 4 \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version llava_llama_2 \
-    --data_path ./dataset/train/dataset.json \
-    --validation_data_path ./dataset/validation/dataset.json \
-    --image_folder ./dataset/images/ \
+    --data_path /localscratch/gna23/LLaVA/dataset/train/dataset.json \
+    --validation_data_path /localscratch/gna23/LLaVA/dataset/validation/dataset.json \
+    --image_folder /localscratch/gna23/LLaVA/dataset/images/ \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -18,7 +18,7 @@ deepspeed ./llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./llava/checkpoints/llama-2-7b-chat-task-qlora \
+    --output_dir /localscratch/gna23/LLaVA/llava/checkpoints/llama-2-7b-chat-task-qlora \
     --num_train_epochs 500 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 32 \
