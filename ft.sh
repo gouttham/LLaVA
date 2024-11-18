6dc1ce7f71ba777fe724711fee3f591328f6e898
gna23 <gna23@sfu.ca>
2024-11-18 11:53:29 -0800

    init

--- ft.sh
+++ ft.sh
@@ -8,7 +8,6 @@ deepspeed /localscratch/gna23/LLaVA/llava/train/train_mem.py \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version llava_llama_2 \
     --data_path /localscratch/gna23/LLaVA/dataset/train/dataset.json \
-    --validation_data_path /localscratch/gna23/LLaVA/dataset/validation/dataset.json \
     --image_folder /localscratch/gna23/LLaVA/dataset/images/ \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
