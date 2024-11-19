cfb382d61b78c46fb305a576cb9ea2bc17a08600
gna23 <gna23@sfu.ca>
2024-11-19 13:39:09 -0800

    init

--- infer.sh
+++ infer.sh
@@ -3,13 +3,13 @@
 #    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
 #    --query "why was this photo taken?"
 
-python ./llava/eval/run_llava.py \
-    --model-path "liuhaotian/llava-v1.5-7b" \
-    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
-    --query "why was this photo taken?"\
-
 #python ./llava/eval/run_llava.py \
-#    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_fn_weights4" \
-#    --model-base "liuhaotian/llava-v1.5-7b" \
+#    --model-path "liuhaotian/llava-v1.5-7b" \
 #    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
-#    --query "why was this photo taken?"
+#    --query "why was this photo taken?"\
+
+python ./llava/eval/run_llava.py \
+    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_fn_weights4" \
+    --model-base "liuhaotian/llava-v1.5-7b" \
+    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
+    --query "why was this photo taken?"
