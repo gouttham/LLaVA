e3442149ac620918704afdbaacd4949b676890d7
gna23 <gna23@sfu.ca>
2024-11-19 12:38:36 -0800

    init

--- infer.sh
+++ infer.sh
@@ -1,5 +1,5 @@
 python ./llava/eval/run_llava.py \
-    --model-path "/localscratch/gna23/LLaVA/downloads/ch_merged/llava_merged/" \
+    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoint_merged/llava_merged/" \
     --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
     --query "why was this photo taken?"\
 
