7988343fa2eba98c6d2d45fa57b263544d0a1436
gna23 <gna23@sfu.ca>
2024-11-19 12:51:09 -0800

    init

--- infer.sh
+++ infer.sh
@@ -2,7 +2,6 @@ python ./llava/eval/run_llava.py \
     --model-path "/localscratch/gna23/LLaVA/downloads/checkpoint_merged/llava_merged2/" \
     --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
     --query "why was this photo taken?"\
-    --tune_mm_mlp_adapter True
 
 #python ./llava/eval/run_llava.py \
 #    --model-path "liuhaotian/llava-v1.5-7b" \
