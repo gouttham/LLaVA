7aff6a5bb4d1efd7a7e015f31ece728c37685e29
gna23 <gna23@sfu.ca>
2024-11-19 11:30:25 -0800

    init

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 python scripts/merge_lora_weights.py \
-    --model-path "checkpoints/llama-2-7b-chat-task-qlora" \
+    --model-path "checkpoints/llava_lora_weights" \
     --model-base "liuhaotian/llava-v1.5-7b" \
     --save-model-path "checkpoints/merged/"
