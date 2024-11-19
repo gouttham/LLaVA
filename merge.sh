4a4e68f7d5df7763ec13423b33020cb9aaeb9c4d
gna23 <gna23@sfu.ca>
2024-11-19 11:33:43 -0800

    init

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 python scripts/merge_lora_weights.py \
     --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_weights" \
     --model-base "liuhaotian/llava-v1.5-7b" \
-    --save-model-path "./checkpoints/llava_merged/"
+    --save-model-path "/localscratch/gna23/LLaVA/checkpoints/llava_merged/"
