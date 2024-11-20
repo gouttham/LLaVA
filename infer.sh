de401d1f3168fb4c331695095bd64fea8d7b35a1
gna23 <gna23@sfu.ca>
2024-11-19 16:20:02 -0800

    init

--- infer.sh
+++ infer.sh
@@ -11,5 +11,5 @@
 python ./llava/eval/run_llava.py \
     --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_weights7" \
     --model-base "liuhaotian/llava-v1.5-7b" \
-    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
+    --image-file "/localscratch/gna23/LLaVA/dataset/images/be76e1f3-4d58-499c-b7ff-1a0aa24f8260.jpg" \
     --query "why was this photo taken?"
