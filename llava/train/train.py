b6c38111325954af41db646e5e7d5865a7164a33
gna23 <gna23@sfu.ca>
2024-11-21 15:36:35 -0800

    debug

--- llava/train/train.py
+++ llava/train/train.py
@@ -821,6 +821,7 @@ class DataCollatorForSupervisedDataset(object):
         labels = self.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
         batch = dict(input_ids=input_ids, labels=labels.long() if labels.dtype == torch.int32 else labels, attention_mask=input_ids.ne(self.tokenizer.pad_token_id))
         # batch = dict(input_ids=input_ids, labels=labels, attention_mask=input_ids.ne(self.tokenizer.pad_token_id), ids=ids)
+        print(instances)
         import pdb;pdb.set_trace()
         if "image" in instances[0]:
             images = [instance["image"] for instance in instances]
