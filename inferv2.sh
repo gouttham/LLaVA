9e387cb6556116166968e931c696f18b33ed11e3
gna23 <gna23@sfu.ca>
2024-11-21 18:06:21 -0800

    debug

---
+++ inferv2.sh
@@ -0,0 +1,15 @@
+#python ./llava/eval/run_llava.py \
+#    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoint_merged/llava_merged2/" \
+#    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
+#    --query "why was this photo taken?"
+
+#python ./llava/eval/run_llava.py \
+#    --model-path "liuhaotian/llava-v1.5-7b" \
+#    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
+#    --query "why was this photo taken?"\
+
+python ./llava/eval/run_llavav2.py \
+    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_weights7" \
+    --model-base "liuhaotian/llava-v1.5-7b" \
+    --image-file "/localscratch/gna23/LLaVA/dataset/images/be763e16-8714-4bf7-8bd5-3d4cc05bd160.jpg" \
+    --query "why was this photo taken?"
