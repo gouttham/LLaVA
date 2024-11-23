ce0cec3683f9fedd5129db9ce81481c69fb14c7d
gna23 <gna23@sfu.ca>
2024-11-23 00:16:28 -0800

    debug

--- ft0.sh
+++ ft0.sh
@@ -17,7 +17,7 @@ deepspeed /localscratch/gna23/LLaVA/llava/train/train_mem2.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_cls2 \
+    --output_dir /localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_cls2_4cls \
     --num_train_epochs 500 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 16 \
--- infer0.sh
+++ infer0.sh
@@ -9,7 +9,7 @@
 #    --query "why was this photo taken?"\
 
 python ./llava/eval/run_llava0.py \
-    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_cls2" \
+    --model-path "/localscratch/gna23/LLaVA/downloads/checkpoints/llava_lora_fn_cls2_4cls" \
     --model-base "liuhaotian/llava-v1.5-7b" \
     --image-file "/localscratch/gna23/LLaVA/v2/cd_images/7fada345-8e1e-4956-a1bb-79735da8928f_no.jpg" \
     --query "Are given two images are similar ?"
